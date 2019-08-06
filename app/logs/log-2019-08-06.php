a4cbdbdf1798fbbd0afc9d4151bad121a537984c
abdul.kader138 <babu313136@gmail.com>
2019-08-07 00:11:21 +0600

    Code cleaning

--- app/logs/log-2019-08-06.php
+++ app/logs/log-2019-08-06.php
@@ -25,3 +25,56 @@ ERROR - 2019-08-06 10:28:24 --> Could not find the language line "Doc_Mov."
 ERROR - 2019-08-06 10:28:24 --> Could not find the language line "File_Exp."
 ERROR - 2019-08-06 10:28:24 --> Could not find the language line "Man._Req."
 ERROR - 2019-08-06 10:28:24 --> Could not find the language line "Corr._Req."
+ERROR - 2019-08-06 20:59:02 --> Query error: Table 'editphoto.sma_approve_details_others' doesn't exist - Invalid query: SELECT count(id) as total
+FROM `sma_approve_details_others`
+WHERE `aprrover_id` = '1'
+AND `approve_status` =0
+AND `interface_name` = 'Correction Request'
+ERROR - 2019-08-06 20:59:02 --> Severity: error --> Exception: Call to a member function num_rows() on boolean C:\xampp\htdocs\editphoto\app\models\Db_model.php 43
+ERROR - 2019-08-06 21:19:27 --> Query error: Table 'editphoto.sma_sessions' doesn't exist - Invalid query: SELECT `data`
+FROM `sma_sessions`
+WHERE `id` = '4jj4vsg93h4kenl86m7jmassoac6gft6'
+ERROR - 2019-08-06 21:19:28 --> Query error: Table 'editphoto.sma_settings' doesn't exist - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2019-08-06 21:19:28 --> Severity: error --> Exception: Call to a member function num_rows() on boolean C:\xampp\htdocs\editphoto\app\models\Site.php 14
+ERROR - 2019-08-06 21:19:28 --> Query error: Table 'editphoto.sma_sessions' doesn't exist - Invalid query: INSERT INTO `sma_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES ('4jj4vsg93h4kenl86m7jmassoac6gft6', '::1', 1565104768, '__ci_last_regenerate|i:1565104767;')
+ERROR - 2019-08-06 21:19:28 --> Severity: Warning --> session_write_close(): Failed to write session data using user defined save handler. (session.save_path: sessions) Unknown 0
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Employee"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Guard"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "File_Manager"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "File_Manager_New"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "HR"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "List_Manpower_Requisition"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Add_Manpower_Requisition"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "List_Recruitment_Approval"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Add_Recruitment_Approval"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Correction_Request"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "List_Correction_Request"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Add_Correction_Request"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Approver_Setup_HR"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Approver_Setup_Others"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Waiting_For_Approval"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Manpower_Requisition"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Recruitment_Approval"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Correction_Request"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Employees"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Bills"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Documents"
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Doc_Mov."
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "File_Exp."
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Man._Req."
+ERROR - 2019-08-06 21:49:26 --> Could not find the language line "Corr._Req."
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Employee"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Guard"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "File_Manager"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "File_Manager_New"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "HR"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "List_Manpower_Requisition"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Add_Manpower_Requisition"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "List_Recruitment_Approval"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Add_Recruitment_Approval"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Correction_Request"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "List_Correction_Request"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Add_Correction_Request"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Approver_Setup_HR"
+ERROR - 2019-08-06 21:50:27 --> Could not find the language line "Approver_Setup_Others"
